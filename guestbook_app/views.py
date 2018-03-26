625a83b8d79832ada22f5394efd61bbcf1837527
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-26 16:33:01 -0400

    Feat: Render the html pages

--- guestbook_app/views.py
+++ guestbook_app/views.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'guestbook_app/index.html')
+
+def sign(request):
+    return render(request, 'guestbook_app/sign.html')
