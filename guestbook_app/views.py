76eb9e7541ccb883bcac88a0cf2016082b94bf82
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-26 22:05:22 -0400

    Feat: Display comments on the guestbook index page

--- guestbook_app/views.py
+++ guestbook_app/views.py
@@ -3,8 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Comment
+
 def index(request):
-    return render(request, 'guestbook_app/index.html')
+    comments = Comment.objects.order_by('-date_added')
+
+    context = {'comments': comments}
+    return render(request, 'guestbook_app/index.html', context)
 
 def sign(request):
     return render(request, 'guestbook_app/sign.html')
