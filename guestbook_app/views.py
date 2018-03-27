d6743d828cf13a80d6e939d4b9fd6af6bcb75670
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-26 22:27:19 -0400

    Feat: Use django forms for sign form

--- guestbook_app/views.py
+++ guestbook_app/views.py
@@ -4,12 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from .models import Comment
+from .forms import CommentForm
 
 def index(request):
     comments = Comment.objects.order_by('-date_added')
-
     context = {'comments': comments}
+
     return render(request, 'guestbook_app/index.html', context)
 
 def sign(request):
-    return render(request, 'guestbook_app/sign.html')
+    form = CommentForm()
+    context = {'form': form}
+    return render(request, 'guestbook_app/sign.html', context)
